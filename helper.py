010095692d3bed9c6ea41669a0b3360de21d4e6e
Helena Wu <helenawu1998@gmail.com>
2018-02-22 23:03:03 -0800

    Fix helper function to include list of genres

--- helper.py
+++ helper.py
@@ -1,49 +1,33 @@
 import numpy as np
 
-def load_data(data_file, train_file, test_file, movies_file, skiprows = 0,
-m_cols = 2):
+def load_data(data_file, movies_file, skiprows = 0, m_cols = 1):
     '''
-    Function loads training and test data and movie and info data from input
-    files and returns x_train, y_train, and x_test in numpy ndarrays.
+    Function loads movie and all ratings data from input
+    files and returns data, genres, movies_data in numpy ndarrays.
 
     Inputs:
     data_file: filename for all ratings data
     movies_file: filename for movies
-    train_file: filename for training ratings data
-    test_file: filename for testing ratings data
     skiprows: number of rows to skip in reading any input file
     m_cols: the column to start loading in movies file
 
     Outputs:
     data: the data from all ratings data file as numpy ndarray
-    X_train: x values for training set as numpy ndarray
-    y_train: labels for x values in training set as numpy ndarray
-    X_test: x values for testing set as numpy ndarray
-    y_test: x values for testing set as numpy ndarray
+    genres: returns a list of genres in the same order as labeled in movies_data
+    movies_data: the data from each movie (removed movie id column)
     '''
-    movies_data = np.genfromtxt('data/movies.txt', delimiter = '\t',dtype= None, names=('movie id', 'movie title', 'unknown',
-                                                    'action', 'adventure','animation','childrens',
-                                                    'comedy', 'crime', 'documentary',
-                                                    'drama', 'fantasy','film-noir','horror',
-                                                    'musical', 'mystery', 'romance','sci-fi',
-                                                    'thriller', 'war', 'western'))
+    genres = np.loadtxt(movies_file, dtype = 'str', skiprows = skiprows,
+    usecols = (m_cols,), delimiter = '\t', encoding="cp1252")
+    movies_data = np.loadtxt(movies_file, skiprows = skiprows, usecols = m_cols + 1,
+    delimiter = '\t', encoding="cp1252")
+
     data = np.loadtxt(data_file, skiprows = skiprows, delimiter = '\t')
-    train_data = np.loadtxt(train_file, skiprows = skiprows, delimiter = '\t')
-    test_data = np.loadtxt(test_file, skiprows = skiprows, delimiter = '\t')
 
-    X_train = train_data[:, :-1]
-    y_train = train_data[:,-1]
-
-    X_test = test_data[:, :-1]
-    y_test = test_data[:,-1]
-
-    return data, X_train, y_train, X_test, y_test, movies_data
+    return data, genres, movies_data
 
 # Example usage of load_data
-'''
-data, X_train, y_train, X_test, y_test, movies_data = load_data('data/data.txt',
-'data/train.txt', 'data/test.txt', 'data/movies.txt')
-'''
+# data, genres, movies_data = load_data('data/data.txt', 'data/movies.txt')
+
 
 def error(y, y_pred):
   '''
