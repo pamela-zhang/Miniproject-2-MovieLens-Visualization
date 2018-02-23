8c4ba7075d1ae3c95ba018195fb70f4f88d11d01
Helena Wu <helenawu1998@gmail.com>
2018-02-23 00:13:31 -0800

    Fix load_data to include genres, and fix movies_data

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def load_data(data_file, movies_file, skiprows = 0, m_cols = 1):
+def load_data(data_file, movies_file, skiprows = 0):
     '''
     Function loads movie and all ratings data from input
     files and returns data, genres, movies_data in numpy ndarrays.
@@ -9,25 +9,27 @@ def load_data(data_file, movies_file, skiprows = 0, m_cols = 1):
     data_file: filename for all ratings data
     movies_file: filename for movies
     skiprows: number of rows to skip in reading any input file
-    m_cols: the column to start loading in movies file
 
     Outputs:
     data: the data from all ratings data file as numpy ndarray
-    genres: returns a list of genres in the same order as labeled in movies_data
+    titles: returns a list of genres in the same order as labeled in movies_data
     movies_data: the data from each movie (removed movie id column)
+    genres: returns a list of the 19 genres in order that movies_data is labeled
     '''
-    genres = np.loadtxt(movies_file, dtype = 'str', skiprows = skiprows,
-    usecols = (m_cols,), delimiter = '\t', encoding="cp1252")
-    movies_data = np.loadtxt(movies_file, skiprows = skiprows, usecols = m_cols + 1,
-    delimiter = '\t', encoding="cp1252")
+    movies = np.loadtxt(movies_file, dtype = 'str', skiprows = skiprows, delimiter = '\t', encoding="cp1252")
+    titles = movies[:, 1]
+    movies_data = movies[:, 2:].astype(float)
+    genres = ["Unknown", "Action", "Adventure", "Animation", "Childrens",
+    "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir",
+    "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller",
+    "War", "Western"]
 
     data = np.loadtxt(data_file, skiprows = skiprows, delimiter = '\t')
 
-    return data, genres, movies_data
+    return data, titles, movies_data, genres
 
 # Example usage of load_data
-# data, genres, movies_data = load_data('data/data.txt', 'data/movies.txt')
-
+data, titles, movies_data, genres = load_data('data/data.txt', 'data/movies.txt')
 
 def error(y, y_pred):
   '''
